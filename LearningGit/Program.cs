f4eafc06db642e5d2c2701fe8b429ac5bd94610c
ka1peshc <kalpesh111997@gmail.com>
2021-09-20 12:02:25 +0530

    Two Integer digit addition

--- LearningGit/Program.cs
+++ LearningGit/Program.cs
@@ -7,6 +7,8 @@ namespace LearningGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int sum = 10 + 20;
+            Console.WriteLine("sum of two number is {0}", sum);
         }
     }
 }
