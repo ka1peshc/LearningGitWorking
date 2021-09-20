fc4c736fdcd4241699a81414860d29dbf90386bf
ka1peshc <kalpesh111997@gmail.com>
2021-09-20 12:18:49 +0530

    Adding one file and subtracting functionality

---
+++ LearningGit/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningGit
+{
+    class Calculator
+    {
+        void testFunction()
+        {
+            Console.WriteLine("This is function");
+        }
+    }
+}
--- LearningGit/Program.cs
+++ LearningGit/Program.cs
@@ -9,6 +9,9 @@ namespace LearningGit
             Console.WriteLine("Hello World!");
             int sum = 10 + 20;
             Console.WriteLine("sum of two number is {0}", sum);
+
+            int sub = 20 - 12;
+            Console.WriteLine("subtraction of two digit is  {0} sum of two digit is {1}", sub,sum);
         }
     }
 }
